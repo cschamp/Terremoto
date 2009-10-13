cae4c9387ee7cf125a9aea9c587f3bf44c421c33
cschamp <cschamp@gmail.com>
2009-10-13 15:03:50 -0700

    Inform the user that we need the Internet if the network is unreacable, rather than showing them a blank map. http://github.com/cschamp/Terremoto/issues/#issue/6

---
+++ Classes/NetAccessViewController.h
@@ -0,0 +1,16 @@
+//
+//  NetAccessViewController.h
+//  Terremoto
+//
+//  Created by Craig Schamp on 10/13/09.
+//  Copyright 2009 Craig Schamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NetAccessViewController : UIViewController {
+	UITextView *mainText;
+	UITextView *subText;
+}
+
+@end
