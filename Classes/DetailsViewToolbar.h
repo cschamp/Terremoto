3620037f4307f35966e48b080651b526575a103c
Craig Schamp <cschamp@vmware.com>
2011-08-14 11:15:09 -0700

    Updated for iOS 5 SDK and Xcode 4.2.

---
+++ Classes/DetailsViewToolbar.h
@@ -0,0 +1,16 @@
+//
+//  DetailsViewToolbar.h
+//  Terremoto
+//
+//  Created by Craig Schamp on 11/16/09.
+//  Copyright 2009 Craig Schamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface DetailsViewToolbar : UIToolbar {
+
+}
+
+@end
---
+++ Classes/DetailsViewToolbar.m
@@ -0,0 +1,19 @@
+//
+//  DetailsViewToolbar.m
+//  Terremoto
+//
+//  Created by Craig Schamp on 11/16/09.
+//  Copyright 2009 Craig Schamp. All rights reserved.
+//
+
+#import "DetailsViewToolbar.h"
+
+
+@implementation DetailsViewToolbar
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    return YES;
+}
+
+
+@end
